7c7c6a10d472c5b0fdceb64cb17f60239b840ba9
tobia <tobia@LAPTOP-C39FJJMG>
2019-03-18 08:55:38 +0100

    test

--- No_Waste_Food_Inventory/src/gui/LoginGUI.java
+++ No_Waste_Food_Inventory/src/gui/LoginGUI.java
@@ -27,7 +27,6 @@ public class LoginGUI extends JFrame {
     
     public LoginGUI() {
         super("Login");
-        System.out.println("test");
         this.pack();
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(new Dimension(500,700));
