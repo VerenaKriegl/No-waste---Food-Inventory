c167dac47e8b988c718831c048e373d9297c78b7
krieg <kriveb15@htlkaindorf.at>
2019-03-21 09:42:03 +0100

    reminder

--- No_Waste_Food_Inventory/src/beans/Reminder.java
+++ No_Waste_Food_Inventory/src/beans/Reminder.java
@@ -1,51 +1,42 @@
 package beans;
 
-public class Reminder
-{
+public class Reminder {
 
     private int amount;
     private String productName, timeunit;
 
-    public Reminder(int amount, String productName, String timeunit)
-    {
+    public Reminder(int amount, String productName, String timeunit) {
         this.amount = amount;
         this.productName = productName;
         this.timeunit = timeunit;
     }
 
-    public int getAmount()
-    {
+    public int getAmount() {
         return amount;
     }
 
-    public void setAmount(int amount)
-    {
+    public void setAmount(int amount) {
         this.amount = amount;
     }
 
-    public String getProductName()
-    {
+    public String getProductName() {
         return productName;
     }
 
-    public void setProductName(String productName)
-    {
+    public void setProductName(String productName) {
         this.productName = productName;
     }
 
-    public String getTimeunit()
-    {
+    public String getTimeunit() {
         return timeunit;
     }
 
-    public void setTimeunit(String timeunit)
-    {
+    public void setTimeunit(String timeunit) {
         this.timeunit = timeunit;
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return "";
     }
 
