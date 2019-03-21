07df069b7fa390ec1b90507b97d473e47586bfd6
krieg <kriveb15@htlkaindorf.at>
2019-03-21 18:19:23 +0100

    server

--- No_Waste_Food_Inventory/src/server/Server.java
+++ No_Waste_Food_Inventory/src/server/Server.java
@@ -15,56 +15,58 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Server 
-{
+public class Server {
+
     private Map<String, ArrayList<Product>> userMap = new HashMap();
-    public static void main(String[] args) 
-    {
+
+    public static void main(String[] args) {
         try {
             new Server();
         } catch (IOException ex) {
             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    public Server() throws IOException
-    {
+
+    public Server() throws IOException {
         ServerSocket serverSocket = new ServerSocket(9999);
         ClientConnection clientConnection = new ClientConnection(serverSocket);
         clientConnection.start();
     }
-    
-    class ClientConnection extends Thread
-    {
+
+    class ClientConnection extends Thread {
+
         private ServerSocket serverSocket;
-        public ClientConnection(ServerSocket ss)
-        {
+
+        public ClientConnection(ServerSocket ss) {
             this.serverSocket = ss;
         }
 
         @Override
-        public void run() 
-        {
+        public void run() {
             try {
                 System.out.println("Server wartet");
-                Socket s = serverSocket.accept();
-                System.out.println("Connected mit: "+s.getRemoteSocketAddress());
-                ClientCommunication clientCommunication = new ClientCommunication(s);
-                clientCommunication.start();
+                while (true) {
+                    Socket s = serverSocket.accept();
+                    System.out.println("Connected mit: " + s.getRemoteSocketAddress());
+                    ClientCommunication clientCommunication = new ClientCommunication(s);
+                    clientCommunication.start();
+                }
             } catch (IOException ex) {
                 Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        
+
     }
-    class ClientCommunication extends Thread
-    {
+
+    class ClientCommunication extends Thread {
+
         private DBAccess dbAccess;
         private Socket socket;
         private ArrayList<Product> listProduct = new ArrayList<>();
         private ObjectInputStream ois;
         private ObjectOutputStream oos;
-        public ClientCommunication(Socket s)
-        {
+
+        public ClientCommunication(Socket s) {
             this.socket = s;
             try {
                 oos = new ObjectOutputStream(s.getOutputStream());
@@ -75,47 +77,36 @@ public class Server
         }
 
         @Override
-        public void run() 
-        {
+        public void run() {
             try {
                 dbAccess = new DBAccess();
-                String line = (String)ois.readObject();
+                String line = (String) ois.readObject();
                 ArrayList<String> userNames = dbAccess.showUser();
-                if(line.contains("Anmelden"))
-                {
-                    String username = (String)ois.readObject();
+                if (line.contains("Anmelden")) {
+                    String username = (String) ois.readObject();
                     String password = dbAccess.getPassword(username);
-                    line = (String)ois.readObject();
-                    if(password.equals(line))
-                    {
-                        oos.writeObject("Willkommen, "+username);
-                        oos.flush();                     
-                    }
-                    else
-                    {
+                    line = (String) ois.readObject();
+                    if (password.equals(line)) {
+                        oos.writeObject("Willkommen, " + username);
+                        oos.flush();
+                    } else {
                         oos.writeObject("Falsches Password");
                         oos.flush();
                     }
-                }
-                else
-                {
+                } else {
                     ArrayList<Product> productList = new ArrayList<>();
-                    User user = (User)ois.readObject();
-                    if(!userNames.contains(user.getUserName()))
-                    {
+                    User user = (User) ois.readObject();
+                    if (!userNames.contains(user.getUserName())) {
                         dbAccess.insertUser(user);
                         userMap.put(line, productList);
-                        oos.writeObject("Herzlich Willkommen in unserer Community, "+user.getUserName());
+                        oos.writeObject("Herzlich Willkommen in unserer Community, " + user.getUserName());
                         oos.flush();
-                    }
-                    else
-                    {
+                    } else {
                         oos.writeObject("Registrieren fehlgeschlagen");
                         oos.flush();
-                    }                       
+                    }
                 }
-                
-                
+
                 listProduct = dbAccess.showAllProducts("test");
             } catch (ClassNotFoundException ex) {
                 Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
@@ -125,11 +116,10 @@ public class Server
                 Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        
+
     }
-    
-    private void log(String message)
-    {
+
+    private void log(String message) {
         System.out.println(message);
     }
 }
